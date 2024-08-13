1439f1c127f211b50c0eab829ff93fd411c02fae
Seth Curry <seth@scurry.io>
2024-08-13 17:38:30 -0500

    Adding toggle for showing/hiding icons from map

--- src/components/GameMap.tsx
+++ src/components/GameMap.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, type ReactElement } from "react";
 
 import {
   ImageOverlay,
@@ -9,8 +9,9 @@ import {
 } from "react-leaflet";
 
 import "leaflet/dist/leaflet.css";
-import { Icon, Map } from "leaflet";
+import { Icon } from "leaflet";
 import type { LatLngExpression } from "leaflet";
+import { Checkbox, Flex } from "antd";
 
 interface NamedLocation {
   popup: string;
@@ -19,17 +20,37 @@ interface NamedLocation {
 
 export interface GameMapProps {
   mapImageURL: string;
-  cashouts: NamedLocation[];
-  spawns: NamedLocation[];
+  markerSets: CustomMarkerMap;
 }
 
 interface CashoutIconProps {
   position: LatLngExpression;
-  popup: string;
+  popup: ReactElement;
 }
 
-function CashoutIcon({ position, popup }: CashoutIconProps) {
-  const [zoomLevel, setZoomLevel] = useState(3);
+interface CustomMarkerProps {
+  iconURL: string;
+  popup: ReactElement;
+  position: LatLngExpression;
+}
+
+interface CustomMarker {
+  popup: string;
+  position: LatLngExpression;
+}
+
+interface CustomMarkerSet {
+  cashouts: CustomMarker[];
+}
+
+interface CustomMarkerMap {
+  [key: string]: CustomMarkerSet;
+}
+
+const defaultZoomLevel = 3;
+
+function CustomMarker({ iconURL, popup, position }: CustomMarkerProps) {
+  const [zoomLevel, setZoomLevel] = useState(defaultZoomLevel);
 
   const mapEvents = useMapEvents({
     zoomend: () => {
@@ -37,44 +58,93 @@ function CashoutIcon({ position, popup }: CashoutIconProps) {
     },
   });
 
-  const cashoutIcon = new Icon({
-    iconUrl: "/map-icons/cashout.png",
+  const icon = new Icon({
+    iconUrl: iconURL,
     iconSize: [10 * zoomLevel, 10 * zoomLevel],
   });
 
   return (
-    <Marker position={position} icon={cashoutIcon}>
+    <Marker position={position} icon={icon}>
       <Popup>{popup}</Popup>
     </Marker>
   );
 }
 
-export default function GameMap({
-  mapImageURL,
-  cashouts,
-  spawns,
-}: GameMapProps) {
+function CashoutIcon({ position, popup }: CashoutIconProps) {
   return (
-    <MapContainer
-      zoom={3}
-      center={[50, 50]}
-      bounds={[
-        [0, 0],
-        [100, 100],
-      ]}
-      style={{ width: "100%", height: "50vh" }}
-    >
-      <ImageOverlay
-        url={mapImageURL}
+    <CustomMarker
+      iconURL="/map-icons/cashout.png"
+      popup={popup}
+      position={position}
+    />
+  );
+}
+
+function MarkersForSet(markerSet: CustomMarkerSet) {
+  return markerSet.cashouts.map((marker) => {
+    return (
+      <CashoutIcon
+        {...marker}
+        position={marker.position}
+        popup={<p>{marker.popup}</p>}
+      />
+    );
+  });
+}
+
+function RenderMarkers(markerSets: CustomMarkerMap, enabledMarkers: string[]) {
+  const enabledMarkerSets = Object.keys(markerSets).filter((key) => {
+    return enabledMarkers.includes(key);
+  });
+
+  return enabledMarkerSets
+    .map((key) => {
+      const markerSet = markerSets[key];
+      return MarkersForSet(markerSet);
+    })
+    .flat(1);
+}
+
+export default function GameMap({ mapImageURL, markerSets }: GameMapProps) {
+  const [enabledMarkers, setEnabledMarkers] = useState<string[]>([]);
+
+  const onSelectedMarkersChanged = (selectedMarkers: string[]) => {
+    setEnabledMarkers(selectedMarkers);
+  };
+
+  return (
+    <Flex style={{ width: "100%", height: "50vh" }}>
+      <MapContainer
+        zoom={defaultZoomLevel}
+        center={[50, 50]}
         bounds={[
           [0, 0],
-          [75, 100],
+          [100, 100],
         ]}
-      />
+        style={{ width: "100%", height: "50vh" }}
+      >
+        <ImageOverlay
+          url={mapImageURL}
+          bounds={[
+            [0, 0],
+            [75, 100],
+          ]}
+        />
 
-      {cashouts.map((loc) => {
-        return <CashoutIcon position={loc.position} popup={loc.popup} />;
-      })}
-    </MapContainer>
+        {...RenderMarkers(markerSets, enabledMarkers)}
+      </MapContainer>
+      <Checkbox.Group
+        style={{ width: "25%", marginLeft: "1em", color: "grey" }}
+        onChange={onSelectedMarkersChanged}
+      >
+        {Object.keys(markerSets).map((name) => {
+          return (
+            <Checkbox value={name} style={{ color: "grey", height: "1.5em" }}>
+              {name}
+            </Checkbox>
+          );
+        })}
+      </Checkbox.Group>
+    </Flex>
   );
 }
